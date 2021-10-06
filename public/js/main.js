4c762f98811b1eb7a6e1d13b19b0893165ab9458
NIVEDITHA54 <niveditharamu.54@gmail.com>
2021-10-06 15:41:15 +0200

    added code for filter category

--- public/js/main.js
+++ public/js/main.js
@@ -66,29 +66,6 @@
   };
 
   /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
-  on(
-    "click",
-    ".scrollto",
-    function (e) {
-      if (select(this.hash)) {
-        e.preventDefault();
-
-        let navbar = select("#navbar");
-        if (navbar.classList.contains("navbar-mobile")) {
-          navbar.classList.remove("navbar-mobile");
-          let navbarToggle = select(".mobile-nav-toggle");
-          navbarToggle.classList.toggle("bi-list");
-          navbarToggle.classList.toggle("bi-x");
-        }
-        scrollto(this.hash);
-      }
-    },
-    true
-  );
-
-  /**
    * Scroll with ofset on page load with hash links in the url
    */
   window.addEventListener("load", () => {
@@ -98,57 +75,6 @@
       }
     }
   });
-
-  /**
-   * Porfolio isotope and filter
-   */
-  window.addEventListener("load", () => {
-    let portfolioContainer = select(".portfolio-container");
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: ".portfolio-item",
-      });
-
-      let portfolioFilters = select("#portfolio-flters li", true);
-
-      on(
-        "click",
-        "#portfolio-flters li",
-        function (e) {
-          e.preventDefault();
-          portfolioFilters.forEach(function (el) {
-            el.classList.remove("filter-active");
-          });
-          this.classList.add("filter-active");
-
-          portfolioIsotope.arrange({
-            filter: this.getAttribute("data-filter"),
-          });
-          portfolioIsotope.on("arrangeComplete", function () {
-            AOS.refresh();
-          });
-        },
-        true
-      );
-    }
-  });
-
-  /**
-   * Skills animation
-   */
-  let skilsContent = select(".skills-content");
-  if (skilsContent) {
-    new Waypoint({
-      element: skilsContent,
-      offset: "80%",
-      handler: function (direction) {
-        let progress = select(".progress .progress-bar", true);
-        progress.forEach((el) => {
-          el.style.width = el.getAttribute("aria-valuenow") + "%";
-        });
-      },
-    });
-  }
 
   /**
    * Animation on scroll
--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,63 +1,73 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import { portfolio } from "../data.js";
 
-export default class Portfolio extends Component {
-  render() {
-    return (
-      <section id="portfolio" className="portfolio">
-        <div className="container" data-aos="fade-up">
-          <div className="section-title">
-            <h2>PROJECTS</h2>
-          </div>
-          <div className="row" data-aos="fade-up" data-aos-delay={100}>
-            <div className="col-lg-12 d-flex justify-content-center">
-              <ul id="portfolio-flters">
-                <li data-filter="*" className="filter-active">
-                  All
-                </li>
-                <li data-filter=".filter-backend">Backend development</li>
-                <li data-filter=".filter-frontend">Frontend development</li>
-              </ul>
-            </div>
-          </div>
-          <div className="row" data-aos="fade-up" data-aos-delay={200}>
-            {portfolio.map((item) => (
-              <div key={item.id} className="col-sm-6">
-                <div class="card border-light mb-5 rounded-3">
-                  <h2
-                    class="card-header p-4
+export default function Portfolio() {
+  const [filterParam, setFilterParam] = useState(["All"]);
+  console.log(filterParam);
+
+  function search(items) {
+    if (filterParam === "All") return items;
+    else return items.filter((item) => item.type === filterParam);
+  }
+
+  return (
+    <section id="portfolio" className="portfolio">
+      <div className="container" data-aos="fade-up">
+        <div className="section-title">
+          <h2>PROJECTS</h2>
+        </div>
+        <div className="col-sm-4 offset-sm-4">
+          <select
+            onChange={(e) => {
+              setFilterParam(e.target.value);
+            }}
+            className="form-select form-select-lg mb-3"
+            aria-label=".form-select-lg example"
+          >
+            <option value="All">All</option>
+            <option value="frontend">Frontend</option>
+            <option value="backend">Backend</option>
+          </select>
+          <span className="focus"></span>
+        </div>
+
+        <div className="row" data-aos="fade-up" data-aos-delay={200}>
+          {search(portfolio).map((item) => (
+            <div key={item.id} className="col-sm-6">
+              <div class="card border-light mb-5 rounded-3">
+                <h2
+                  class="card-header p-4
                   "
+                >
+                  {item.title}
+                </h2>
+                <div class="card-body">
+                  <p class="card-subtitle mb-2 text-muted">
+                    <strong>Company :</strong> {item.company}
+                  </p>
+                  <p class="card-subtitle mb-2 text-muted">
+                    <strong>Technologies : </strong> {item.technologies}
+                  </p>
+                  <p class="card-subtitle mb-2 text-muted">
+                    <strong>Project date : </strong>
+                    {item.date}
+                  </p>
+                  <p class="card-text">{item.description}</p>
+                  <a
+                    href={item.url}
+                    className="btn-about"
+                    target="_blank"
+                    rel="noreferrer"
                   >
-                    {item.title}
-                  </h2>
-                  <div class="card-body">
-                    <p class="card-subtitle mb-2 text-muted">
-                      <strong>Company :</strong> {item.company}
-                    </p>
-                    <p class="card-subtitle mb-2 text-muted">
-                      <strong>Technologies : </strong> {item.technologies}
-                    </p>
-                    <p class="card-subtitle mb-2 text-muted">
-                      <strong>Project date : </strong>
-                      {item.date}
-                    </p>
-                    <p class="card-text">{item.description}</p>
-                    <a
-                      href={item.url}
-                      className="btn-about"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Project link
-                    </a>
-                  </div>
+                    Project link
+                  </a>
                 </div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
 }
