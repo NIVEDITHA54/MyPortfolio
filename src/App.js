64a17a1a743bbafc047478610266551cf6776596
NIVEDITHA54 <niveditharamu.54@gmail.com>
2021-10-12 10:14:59 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import HomePage from "./pages/home";
 import About from "./pages/about";
 import Resume from "./pages/resume";
@@ -13,18 +18,18 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Router>
-          <header id="header" className="fixed-top">
-            <Navigation />
-          </header>
-          <Switch>
-            <Route path="/" exact component={() => <HomePage />} />
-            <Route path="/about" exact component={() => <About />} />
-            <Route path="/contact" exact component={() => <Contact />} />
-            <Route path="/resume" exact component={() => <Resume />} />
-            <Route path="/portfolio" exact component={() => <Portfolio />} />
-          </Switch>
-        </Router>
+        <header id="header" className="fixed-top">
+          <Navigation />
+        </header>
+        <Switch>
+          <Route exact path="/" component={() => <HomePage />} />
+
+          <Route path="/about" component={() => <About />} />
+          <Route path="/contact" component={() => <Contact />} />
+          <Route path="/resume" component={() => <Resume />} />
+          <Route path="/portfolio" component={() => <Portfolio />} />
+        </Switch>
+
         <Footer />
       </div>
     );
